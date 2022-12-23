a55790028999c916779a74d39f123ed2fb179dde
sam <sam-chen@eup.com.tw>
2022-12-23 17:35:44 +0800

    feat: update new draw

--- dataProcess.jl
+++ dataProcess.jl
@@ -8,6 +8,13 @@ function draw(stick::Vector{Float64}, sonic::Vector{Float64}, car::String, date:
 	savefig("./" * car * "_" * date * ".png")
 end
 
+function drawByCar(heights::Vector{Vector{Float64}}, car::String, date::Vector{String}, no::String)
+	plot(x, xlabel = "before & after", heights, ylabel = "oil percentage", title = car, linewidth = 3, label = date)
+	plot!(lengend = :outerbottom, legendcolumns = length(date))
+	savefig("byCar/" * car * no * ".png")
+end
+
+
 function writeData(columnCar::Vector{String}, columnDate::Vector{String}, columnStickSlope::Vector{Float64}, columnSonicSlope::Vector{Float64}, columnGape::Vector{Float64})
 	df = DataFrame(Car = columnCar, Date = columnDate, Stick = columnStickSlope, Sonic = columnSonicSlope, Gape = columnGape)
 	CSV.write("./slope.csv", df)
--- plot.jl
+++ plot.jl
@@ -13,36 +13,76 @@ const y1denominator::Float64 = 4095
 const x = ["Before", "After"]
 p = Parameters()
 
-cars::Vector{String} = []
-dates::Vector{String} = []
-sticks::Vector{Float64} = []
-sonics::Vector{Float64} = []
-gape::Vector{Float64} = []
+# cars::Vector{String} = []
+# dates::Vector{String} = []
+# sticks::Vector{Float64} = []
+# sonics::Vector{Float64} = []
+# gape::Vector{Float64} = []
+
+#=
+========================================================
+Ploting by date
+=#
 
 # for (car, boxH) in p.height
 # 	for (date, data) in p.data[car]
 # 		converse(data[1], data[2], data[3], boxH)
 # 		draw(data[1], data[2], car, date)
-# 		push!(cars, car)
-# 		push!(dates, date)
-# 		push!(sticks, data[1][2] - data[1][1])
-# 		push!(sonics, data[2][2] - data[2][1])
-# 		push!(gape, data[1][2] - data[1][1] - data[2][2] + data[2][1])
+# 		# push!(cars, car)
+# 		# push!(dates, date)
+# 		# push!(sticks, data[1][2] - data[1][1])
+# 		# push!(sonics, data[2][2] - data[2][1])
+# 		# push!(gape, data[1][2] - data[1][1] - data[2][2] + data[2][1])
 # 	end
 # end
 
+#=
+========================================================
+=#
+
+#=
+========================================================
+Ploting by car
+=#
+
+for (car, boxH) in p.height
+	stick::Vector{Vector{Float64}} = []
+	sonic::Vector{Vector{Float64}} = []
+	dates::Vector{String} = []
+
+	for (date, data) in p.data[car]
+		converse(data[1], data[2], data[3], boxH)
+		push!(stick, data[1])
+		push!(sonic, data[2])
+		push!(dates, date)
+	end
+
+	drawByCar(stick, car, dates, "1")
+	drawByCar(sonic, car, dates, "2")
+
+end
+
+#=
+========================================================
+=#
+
+
 # writeData(cars, dates, sticks, sonics, gape)
 
-testCar = "50H-06422"
-boxH::Float64 = p.height[testCar]
+# testCar = "50H-06422"
+# date = "1207"
+# boxH::Float64 = p.height[testCar]
 
-boxH -= 3.5
+# boxH -= 3.5
 
-dt = p.data[testCar]
+# dt = p.data[testCar]
+# # dt = p.data[testCar][date]
 
-for (date, data) in dt
-	converse(data[1], data[2], data[3], boxH)
-	plot(x, [data[1] data[2]], title = testCar * "_" * date * "_adjust", linewidth = 3, label = ["stick" "sonic"])
-	plot!(legend = :outerbottom, legendcolumns = 2)
-	savefig("adjust/" * testCar * "_" * date * "_adjust.png")
-end
+
+# for (date, data) in dt
+# 	converse(data[1], data[2], data[3], boxH)
+# 	plot(x, [data[1] data[2]], title = testCar * "_" * date * "_adjust", linewidth = 3, label = ["stick" "sonic"])
+# 	plot!(legend = :outerbottom, legendcolumns = 2)
+# 	savefig("adjust/" * testCar * "_" * date * "_adjust.png")
+# end
+
