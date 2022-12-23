5261014a8c684d9cdd0ca4cf283045f77a050ab0
sam <sam-chen@eup.com.tw>
2022-12-23 14:22:16 +0800

    feat: update 1223

--- plot.jl
+++ plot.jl
@@ -10,34 +10,36 @@ const y1denominator::Float64 = 4095
 const x = ["Before", "After"]
 p = Parameters()
 
-cars = []
-dates = []
-sticks = []
-sonics = []
+cars::Vector{String} = []
+dates::Vector{String} = []
+sticks::Vector{Float64} = []
+sonics::Vector{Float64} = []
+gape::Vector{Float64} = []
 
 for (car, boxH) in p.height
 	for (date, data) in p.data[car]
 		converse(data[1], data[2], data[3], boxH)
-		# draw(data[1], data[2], car, date)
-		push!(cars, car)
-		push!(dates, date)
-		push!(sticks, data[1][2] - data[1][1])
-		push!(sonics, data[2][2] - data[2][1])
+		draw(data[1], data[2], car, date)
+		# push!(cars, car)
+		# push!(dates, date)
+		# push!(sticks, data[1][2] - data[1][1])
+		# push!(sonics, data[2][2] - data[2][1])
+		# push!(gape, data[1][2] - data[1][1] - data[2][2] + data[2][1])
 	end
 end
 
-writeData(cars, dates, sticks, sonics)
+# writeData(cars, dates, sticks, sonics, gape)
 
-# testCar = "50H-06422"
+# testCar = "51D-62952"
 # boxH::Float64 = p.height[testCar]
 
-# boxH -= 3.5
+# boxH -= 3
 
 # dt = p.data[testCar]
 
 # for (date, data) in dt
 # 	converse(data[1], data[2], data[3], boxH)
-# 	plot(x, [data[1] data[2]], title = testCar * "_test", linewidth = 3, label = ["stick" "sonic"])
+# 	plot(x, [data[1] data[2]], title = testCar * "_" * date * "_adjust", linewidth = 3, label = ["stick" "sonic"])
 # 	plot!(legend = :outerbottom, legendcolumns = 2)
-# 	savefig("./" * testCar * "_" * date * "_test.png")
+# 	savefig("adjust/" * testCar * "_" * date * "_adjust.png")
 # end
